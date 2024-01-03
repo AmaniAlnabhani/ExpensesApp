3c470a356026c42943824ccb733d2f12c0192675
AmaniALnabhani <amanialnabhani7@gmail.com>
2024-01-03 19:52:27 +0400

    add expense

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,21 +1,38 @@
 import 'package:flutter/material.dart';
 
-class Expenses extends StatefulWidget{
- const Expenses({super.key});
-@override
+import './expenses_list.dart';
+import 'models/expense.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+  @override
   State<StatefulWidget> createState() {
-   return _ExpensesState();
+    return _ExpensesState();
   }
 }
-class _ExpensesState extends State<Expenses>{
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'flutter course',
+      amount: 19.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: 'cinema',
+      amount: 15.69,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children:const [
-          Text('The Chart'),
-          Text('list'),
-
+        children: [
+          const Text('The Chart'),
+          Expanded(child: ExpensesList(expenses: _registeredExpenses))
         ],
       ),
     );
